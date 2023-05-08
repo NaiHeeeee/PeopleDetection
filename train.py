50f0168074e84d572dc2b9a0a96b1e6b510c98a4
NAIHE <2794096231@qq.com>
2023-05-08 11:19:12 +0800

    训练图片

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def train():
 
     # 设置训练集和验证集的路径
     file_dir = os.path.dirname(__file__)
-    train_data_dir = os.path.join(file_dir, 'face_pictures/train/')
+    train_data_dir = os.path.join(file_dir, 'face_pictures/train_pictures/')
     val_data_dir = os.path.join(file_dir, 'face_pictures/val/')
 
     # 使用ResNet50模型，去掉最后一层，并添加一个全连接层，作为新的输出层
