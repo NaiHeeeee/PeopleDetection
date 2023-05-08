d998897c684a2aad80d9fcf22f6707a3638da1a0
NAIHE <2794096231@qq.com>
2023-05-08 11:18:19 +0800

    训练人脸识别模型

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ def train():
 
     # 设置训练集和验证集的路径
     file_dir = os.path.dirname(__file__)
-    train_data_dir = os.path.join(file_dir, 'face_picture/train/')
-    val_data_dir = os.path.join(file_dir, 'face_picture/val/')
+    train_data_dir = os.path.join(file_dir, 'face_pictures/train/')
+    val_data_dir = os.path.join(file_dir, 'face_pictures/val/')
 
     # 使用ResNet50模型，去掉最后一层，并添加一个全连接层，作为新的输出层
     base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
