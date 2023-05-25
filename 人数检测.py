41a27c2ad299c62013ab256cf4ae2e86b6c84ce1
NAIHE <2794096231@qq.com>
2023-05-25 20:34:44 +0800

    人数检测

--- 人数检测.py
+++ 人数检测.py
@@ -11,6 +11,12 @@ from keras.models import load_model
 class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui_MainWindow, self).__init__()
+
+        # 加载模型
+        self.face_detection_model = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        model_file = os.path.join(os.path.dirname(__file__), 'trained_model/face_detection_model.h5')
+        self.face_recognition_model = load_model(model_file)
+
         self.statusbar = None
         self.lineEdit = None
         self.textBrowser = None
@@ -24,11 +30,11 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.label_4 = None
         self.cwd = None
 
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1244, 750)
+    def setupUi(self, mainWindow):
+        mainWindow.setObjectName("MainWindow")
+        mainWindow.resize(1244, 750)
 
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget = QtWidgets.QWidget(mainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -39,14 +45,14 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.label_2.setGeometry(QtCore.QRect(650, 10, 581, 451))
         self.label_2.setObjectName("label_2")
 
-        self.label_4 = QtWidgets.QLabel("Label 4", self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(10, 490, 101, 31))
-        self.label_4.setObjectName("label_4")
-
         self.label_3 = QtWidgets.QLabel("Label 3", self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(740, 570, 81, 41))
         self.label_3.setFont(QtGui.QFont("Arial", 20, italic=False))
         self.label_3.setObjectName("label_3")
+
+        self.label_4 = QtWidgets.QLabel("Label 4", self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(10, 490, 101, 31))
+        self.label_4.setObjectName("label_4")
 
         self.pushButton = QtWidgets.QPushButton("Button 1", self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(40, 560, 221, 61))
@@ -67,30 +73,31 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.lineEdit.setGeometry(QtCore.QRect(120, 490, 421, 31))
         self.lineEdit.setObjectName("lineEdit")
 
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        mainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(mainWindow)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        mainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(mainWindow)
+        QtCore.QMetaObject.connectSlotsByName(mainWindow)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "图像处理：人数检测"))
-        self.label.setText(_translate("MainWindow", "原图"))
-        self.label_2.setText(_translate("MainWindow", "检测后"))
-        self.label_3.setText(_translate("MainWindow", "人数："))
-        self.label_4.setText(_translate("MainWindow", "图片所在路径："))
-        self.pushButton.setText(_translate("MainWindow", "选择图片"))
+    def retranslateUi(self, mainWindow):
+        _ = QtCore.QCoreApplication.translate
+
+        mainWindow.setWindowTitle(_("MainWindow", "图像处理:人数检测"))
+        self.label.setText(_("MainWindow", "原图"))
+        self.label_2.setText(_("MainWindow", "检测后"))
+        self.label_3.setText(_("MainWindow", "人数:"))
+        self.label_4.setText(_("MainWindow", "图片所在路径:"))
+        self.pushButton.setText(_("MainWindow", "选择图片"))
         self.pushButton.clicked.connect(self.display_orginal)
-        self.pushButton_2.setText(_translate("MainWindow", "人数检测"))
+        self.pushButton_2.setText(_("MainWindow", "人数检测"))
         self.pushButton_2.clicked.connect(self.pepple_detection)
 
     def display_orginal(self):
         file_dir = os.path.dirname(__file__)
         self.cwd = os.path.join(file_dir, 'detected_pictures')
-        filename = QFileDialog.getOpenFileName(self, "选取要检测的图片", self.cwd, "Image Files (*.jpg)")
+        filename = QFileDialog.getOpenFileName(self.centralwidget, "选取要检测的图片", self.cwd, "Image Files (*.jpg)")
         if filename[0] == "":
             print("\n取消选择")
             return
@@ -101,24 +108,15 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
             self.label.setPixmap(picture)
             self.label.setScaledContents(True)
         else:
-            QMessageBox.warning(self, "警告", "图片不存在！")
-
+            print("\n图片不存在")
 
     def pepple_detection(self):
-        # 加载人脸检测模型
-        face_detection_model = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-        # 加载人脸识别模型
-        file_dir = os.path.dirname(__file__)
-        model_file = os.path.join(file_dir, 'trained_model/face_detection_model.h5')
-        face_recognition_model = load_model(model_file)
-
         # 加载图片
         img = cv2.imread(self.lineEdit.text())
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         # 检测人脸
-        faces = face_detection_model.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
+        faces = self.face_detection_model.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
 
         # 统计人脸数
         count = len(faces)
@@ -135,7 +133,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
             face = np.expand_dims(face, axis=0)
 
             # 使用模型预测人脸数量
-            count = int(face_recognition_model.predict(face)[0][0])
+            int(self.face_recognition_model.predict(face)[0][0])
 
             # 在人脸部分画框
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
